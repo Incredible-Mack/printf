657027e544131db38ad5980e818e5f9cddf30ea8
Mack Emma <72084620+Incredible-Mack@users.noreply.github.com>
2023-06-28 01:16:45 +0100

    Add files via upload

--- printf.c
+++ printf.c
@@ -1,65 +1,92 @@
 #include "main.h"
 
+
+/**
+ * handle_decimal - handle  decimal d and i
+ * @num: checks for positive or negative num
+ * @characterCount: this a pointer
+ * Return:  count on success
+ */
+
+int handle_decimal(int num, int *characterCount)
+{
+int digitCount = 0;
+
+if (num < 0)
+{
+handle_char('-', characterCount);
+num = -num;
+digitCount++;
+}
+
+if (num == 0)
+{
+handle_char('0', characterCount);
+digitCount++;
+}
+else
+{
+int reversedNum = 0;
+
+while (num != 0)
+{
+reversedNum = reversedNum * 10 + num % 10;
+num /= 10;
+digitCount++;
+}
+
+while (reversedNum != 0)
+{
+int digit = reversedNum % 10;
+handle_char('0' + digit, characterCount);
+reversedNum /= 10;
+}
+}
+
+return (*characterCount);
+}
+
 /**
  * _printf - handle format conversion
  * @format: this is a pointer
  * Return: the number of character on success
  */
 
-
-int _printf(const char *format, ...) /*handling format*/
+int _printf(const char *format, ...)
 {
 int characterCount = 0;
 va_list args;
 va_start(args, format);
 
-while (*format != '\0') /*while not zero*/
+
+while (*format != '\0')
 {
 if (*format == '%')
 {
 format++;
 if (*format == '%')
 {
-characterCount = handle_char('%', &characterCount);
+handle_char('%', &characterCount);
 }
 else if (*format == 'c')
 {
-char c = va_arg(args, int);
-characterCount = handle_char(c, &characterCount);
+char c = (char)va_arg(args, int);
+handle_char(c, &characterCount);
 }
 else if (*format == 's')
 {
 char *s = va_arg(args, char *);
-characterCount = handle_string(s, &characterCount);
+handle_string(s, &characterCount);
 }
 else if (*format == 'd' || *format == 'i')
 {
 int num = va_arg(args, int);
-int digitCount = 0;
-if (num < 0)
-{
-characterCount = handle_char('-', &characterCount);
-num = -num;
-digitCount++;
-}
-if (num == 0)
-{
-characterCount = handle_char('0', &characterCount);
-digitCount++;
-}
-while (num > 0)
-{
-int digit = num % 10;
-characterCount = handle_char('0' + digit, &characterCount);
-num /= 10;
-digitCount++;
-}
-characterCount += digitCount;
+handle_decimal(num, &characterCount);
 }
 }
 else
 {
-characterCount = handle_char(*format, &characterCount);
+handle_char(*format, &characterCount);
 }
 format++;
 }
@@ -68,4 +95,3 @@ va_end(args);
 
 return (characterCount);
 }
-
